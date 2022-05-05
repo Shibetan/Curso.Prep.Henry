da3871bab5c6d427d2959c1efe5ec6c3a546c35e
Rosario Alvarez <shibetan@gmail.com>
2022-05-05 11:41:48 -0500

    tarea 7 done

--- 07-JS-VI/homework/homework.js
+++ 07-JS-VI/homework/homework.js
@@ -39,7 +39,7 @@ function forEach(array, cb) {
   // Pista: Estarás invocando a `cb` varias veces (una por cada valor en la matriz)
   //Tu código:
   array.forEach(function(el, index) {
-  cb(el);
+    cb(el);
   });
   //for(var i = 0; i < array.length; i++) {
   //  cb(array[i]);
@@ -51,9 +51,7 @@ function map(array, cb) {
   // Itera sobre cada valor en "array", pásalo a `cb` y luego ubicar el valor devuelto por `cb` en un nuevo array
   // El nuevo array debe tener la misma longitud que el array del argumento
   //Tu código:
-  for(let i=0;i<array.length;i++){
-    cb(array[i]);
-  }
+
 
   var nuevoArray = array.map(function(el) {
     return cb(el);
@@ -65,6 +63,9 @@ function filter(array) {
   //Filtrar todos los elementos del array que comiencen con la letra "a".
   //Devolver un nuevo array con los elementos que cumplen la condición
   //Tu código:
+  return array.filter(elemento=>{
+    return elemento[0]==='a'
+  })
 }
 
 // No modificar nada debajo de esta línea
